4088d8bbf013711fa05940af680443deb57fbf0e
Anatolii Medvedchuk <sirius.nau@gmail.com>
2015-05-08 17:54:51 +0300

    update sandbox for course 5

--- 05_general/sandbox_05.R
+++ 05_general/sandbox_05.R
@@ -24,3 +24,34 @@ plot(hCluster)
 #New clustering
 hClusterUpdated = hclust(dist(t(log10(trainSpam[, 1:55] + 1))))
 plot(hClusterUpdated)
+
+################ Statistical prediction/modeling Statistical prediction/modeling
+trainSpam$numType = as.numeric(trainSpam$type) - 1
+costFunction = function(x, y) sum(x != (y > 0.5))
+cvError = rep(NA, 55)
+library(boot)
+
+##########
+#lmFormula = reformulate(names(trainSpam)[1], response = "numType")
+#glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+#cvError[1] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
+##########
+
+for (i in 1:55) {
+  lmFormula = reformulate(names(trainSpam)[i], response = "numType")
+  glmFit = glm(lmFormula, family = "binomial", data = trainSpam)
+  cvError[i] = cv.glm(trainSpam, glmFit, costFunction, 2)$delta[2]
+}
+## Which predictor has minimum cross-validated error?
+names(trainSpam)[which.min(cvError)]
+
+################ Get a measure of uncertainty Get a measure of uncertainty
+## Use the best model from the group
+predictionModel = glm(numType ~ charDollar, family = "binomial", data = trainSpam)
+## Get predictions on the test set
+predictionTest = predict(predictionModel, testSpam)
+predictedSpam = rep("nonspam", dim(testSpam)[1])
+## Classify as `spam' for those with prob > 0.5
+predictedSpam[predictionModel$fitted > 0.5] = "spam"
+
+
