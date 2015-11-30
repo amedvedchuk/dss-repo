1073042b1336ee0adb320cf4fd309737c6b79e1f
unknown <sirius.nau@gmail.com>
2015-11-30 17:30:40 +0100

    update ML files for problem #1

--- ml_try/a_ml_model_finding.R
+++ ml_try/a_ml_model_finding.R
@@ -1,7 +1,11 @@
 
 source("a_ml_functions.R")
 
-dt <- preProcess(readData, preBasic, preLog, makeParts_006, reduceTrain_rf006_log_20)
+# dt <- preProcess(readData, preBasic, preLog, makeParts_006, reduceTrain_rf006_log_20)
+# dt <- preProcess(readData, preBasic, makeParts_006, reduceTrain_rf006_log_20)
+
+dt <- preProcess(readData, preBasic, makeParts_006)
+
 
 testMethods(dt, c("glm","rpart", "enet"))
 testMethods(dt, c("enet"))
@@ -29,10 +33,33 @@ testMethods(dt, c("enet"))
 
 # "ownn", "pam", "parRF", "PART", "partDSA", "pcaNNet", "pda", "pda2", 
 # "RSimca", "sda", "sddaLDA", "sddaQDA", "sdwd", "simpls", 
+# "slda", "smda", "snn", "sparseLDA", "svmPoly", "svmRadial", 
+# "svmRadialCost", "svmRadialWeights"
+
+# "rf", "glm", "rpart", "rpart2", "AdaBag", "gbm", "bagFDA", "gcvEarth", 
+# "ranger", "earth", "fda", "xgbTree", "xgbLinear", "C5.0", "RRFglobal", 
+# "treebag", "AdaBoost.M1", "J48", "OneR", "JRip", "extraTrees", "RRF", 
+# "LMT", "C5.0Tree", "C5.0Rules", "ctree2", "blackboost", "gamboost", 
+# "glmboost", "rotationForest", "rotationForestCp", "rFerns", "dwdLinear", 
+# "LogitBoost", "plr", "lda", "ada", "svmLinear", "nb", "bayesglm", 
+# "avNNet", "bstTree", "bagEarth", "bagEarthGCV", "BstLm", "bstSm", 
+# "nodeHarvest", "bagFDAGCV", "bdk", "CSimca", "dnn", "elm", "gamLoess", 
+# "gamSpline", "glmnet", "glmStepAIC", "hda", "hdda", "kernelpls", "knn", 
+# "pam", "parRF", "PART", "partDSA", "RSimca", "sda", "sdwd", "simpls", 
+# "slda", "sparseLDA", "svmPoly", "svmRadial", "svmRadialCost", "svmRadialWeights"
 
 testMethods(dt, c(
-                  "slda", "smda", "snn", "sparseLDA", "svmPoly", "svmRadial", 
-                  "svmRadialCost", "svmRadialWeights"
+                  "rf", "glm", "rpart", "rpart2", "AdaBag", "gbm", "bagFDA", "gcvEarth", 
+                  "ranger", "earth", "fda", "xgbTree", "xgbLinear", "C5.0", "RRFglobal", 
+                  "treebag", "AdaBoost.M1", "J48", "OneR", "JRip", "extraTrees", "RRF", 
+                  "LMT", "C5.0Tree", "C5.0Rules", "ctree2", "blackboost", "gamboost", 
+                  "glmboost", "rotationForest", "rotationForestCp", "rFerns", "dwdLinear", 
+                  "LogitBoost", "plr", "lda", "ada", "svmLinear", "nb", "bayesglm", "avNNet", 
+                  "bstTree", "bagEarth", "bagEarthGCV", "BstLm", "bstSm", "nodeHarvest", 
+                  "bagFDAGCV", "bdk", "CSimca", "dnn", "elm", "gamLoess", "gamSpline", 
+                  "glmnet", "glmStepAIC", "hda", "hdda", "kernelpls", "knn", "pam", "parRF", 
+                  "PART", "partDSA", "RSimca", "sda", "sdwd", "simpls", "slda", "sparseLDA", 
+                  "svmPoly", "svmRadial", "svmRadialCost", "svmRadialWeights"
                   ))
 
 
