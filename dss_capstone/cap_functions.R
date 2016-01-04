bafd9c6f881a976b3bd40ab383b68ff79b7b2a27
Anatolii Medvedchuk <sirius.nau@gmail.com>
2016-01-05 00:50:09 +0200

    add batch processing for dfm

--- dss_capstone/cap_functions.R
+++ dss_capstone/cap_functions.R
@@ -32,8 +32,28 @@ predict_next <- function(dt, phrase, is_hash = FALSE){
     #     }
 }
 
+make_ngrams_batch <- function(nbatches = 1, subject, ...){
+    
+    do_work_batched(nbatches, subject, 
+                    function(row, start_time,  ...){
+                        print(row)
+                        wfm <- make_dfm(subject[row[1]:row[2]], ...)
+                        file_name <- paste("wfm_",format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
+                        print(paste("save batch to file: ", file_name))
+                        saveRDS(wfm, file_name)
+                        # i <<- i+1
+                        NULL
+                    }, ...)
+}
 
-make_ngrams <- function(lines, ngram = 2){
+make_ngrams <- function(lines, ngram){
+    replaced <- make_dfm(lines, ngram)
+    print("start col sums...")
+    replaced <- colSums(replaced)
+    print("done!")
+}
+
+make_dfm <- function(lines, ngram){
     
     to_print <- system.time({
         # remove non ASCII
@@ -52,18 +72,18 @@ make_ngrams <- function(lines, ngram = 2){
         # tokenise to sentences
         replaced <- unlist(tokenize(replaced, what = "sentence"))
         print(Sys.time())
-#         print("replace punctuation")
-#         replaced <- stri_replace_all(replaced, "", regex = "[[:punct:]]+")
-#         print(Sys.time())
-#         print("replace non ASCII again")
-#         replaced <- stri_replace_all(replaced, "", regex = "[[:space:]]+[a-zA-Z]*([^A-Za-z \\d[:punct:]]+[a-zA-Z]*)+")
-#         print(Sys.time())
+        #         print("replace punctuation")
+        #         replaced <- stri_replace_all(replaced, "", regex = "[[:punct:]]+")
+        #         print(Sys.time())
+        #         print("replace non ASCII again")
+        #         replaced <- stri_replace_all(replaced, "", regex = "[[:space:]]+[a-zA-Z]*([^A-Za-z \\d[:punct:]]+[a-zA-Z]*)+")
+        #         print(Sys.time())
         print(cat("sentences length: ", length(replaced)))
         print("head for sentences:")
         print(head(replaced, 20))
         
         print("tokenize to ngrams...")
-        tokens <- tokenize(toLower(replaced), removeNumbers = TRUE, removeTwitter = TRUE, removePunct = T, ngrams = 3)
+        tokens <- tokenize(toLower(replaced), removeNumbers = TRUE, removeTwitter = TRUE, removePunct = T, ngrams = ngram)
         print("head for tokens")
         print(head(tokens, 30))
         
@@ -74,36 +94,75 @@ make_ngrams <- function(lines, ngram = 2){
         print("top features for DFM:")
         print(topfeatures(replaced, 30))
         
-        print("start col sums...")
-        replaced <- colSums(replaced)
-        print("done!")
     })
     print(to_print)
     replaced
 }
 
-make_table_batch <- function(nbatches = 1, ngrams, ...){
-    batches <- c(0, round(1:nbatches*(length(ngrams)/nbatches)))
+merge_word_fm <- function(dfm1, dfm2){
+    if(is.dfm(dfm1)){
+        dfm1 <- colSums(dfm1)
+    }
+    if(is.dfm(dfm2)){
+        dfm2 <- colSums(dfm2)
+    }
+   
+    ind <- intersect(names(dfm1), names(dfm2))
+    diff1 <- setdiff(names(dfm1), names(dfm2))
+    diff2 <- setdiff(names(dfm2), names(dfm1))
+    dmf <- c(dfm1[ind]+dfm2[ind], dfm1[diff1], dfm2[diff2])
+    dmf
+}
+
+do_work_batched <- function(nbatches = 1, subject, FUN, ...){
+    batches <- c(0, round(1:nbatches*(length(subject)/nbatches)))
     # batches <- c(0, round(1:3*(1500/3)))
     start_time <- Sys.time()
     
-    batches <- data.frame(start = batches[1:nbatches]+1, end=batches[2:(nbatches+1)])
+    batches <- data.frame( start = batches[1:nbatches]+1, end=batches[2:(nbatches+1)], idx = 1:nbatches)
     print(batches)
     
-#     for (i in seq_along(batches) ) {
-#         foo.squared[i] = foo[i]^2
-#     }
-    i <- 1
-    apply(batches, 1, function(row){
-        print(row)
-        dt <- make_table(ngrams[row[1]:row[2]], ...)
-        file_name <- paste("dt_",format(start_time, "%Y-%m-%d_%H_%M_"), i, ".rds", sep = "")
-        print(paste("save batch to file: ", file_name))
-        saveRDS(dt, file_name)
-        i <<- i+1
-        NULL
-    })
+    apply(batches, 1, FUN, start_time, ...)
+}
+
+make_table_batch <- function(nbatches = 1, subject, ...){
+    #     batches <- c(0, round(1:nbatches*(length(subject)/nbatches)))
+    #     # batches <- c(0, round(1:3*(1500/3)))
+    #     start_time <- Sys.time()
+    #     
+    #     batches <- data.frame(start = batches[1:nbatches]+1, end=batches[2:(nbatches+1)])
+    #     print(batches)
+    #    
+    #     i <- 1
+    #     apply(batches, 1, function(row){
+    #         print(row)
+    #         dt <- make_table(subject[row[1]:row[2]], ...)
+    #         file_name <- paste("dt_",format(start_time, "%Y-%m-%d_%H_%M_"), i, ".rds", sep = "")
+    #         print(paste("save batch to file: ", file_name))
+    #         saveRDS(dt, file_name)
+    #         i <<- i+1
+    #         NULL
+    #     })
+    do_work_batched(nbatches, subject, 
+                    function(row, start_time,  ...){
+                        print(row)
+                        dt <- make_table(subject[row[1]:row[2]], ...)
+                        file_name <- paste("dt_",format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
+                        print(paste("save batch to file: ", file_name))
+                        saveRDS(dt, file_name)
+                        # i <<- i+1
+                        NULL
+                    }, ...)
     
+}
+
+read_batched_wfm <- function(prefix){
+    files <- grep(prefix, list.files("."), value = T)
+    dt <- readRDS(files[1])
+    sapply(files[2:length(files)], function(file){
+        dt <<- merge_word_fm(dt, readRDS(file))
+    })
+    dt
 }
 
 read_batched_table <- function(prefix){
--- dss_capstone/run_performance.R
+++ dss_capstone/run_performance.R
@@ -1,10 +1,18 @@
 
 source("cap_functions.R")
 
+# ngrams <- readRDS("ngrams_2016_01_03.rds")
+
+total <- c(read_file("final/en_US/en_US.blogs.txt"),
+           read_file("final/en_US/en_US.news.txt"),
+           read_file("final/en_US/en_US.twitter.txt"))
+
+make_ngrams_batch(nbatches = 5, total, ngram = 3)
+
+ng <- read_batched_wfm("wfm_2016-01-05_00_04")
 
 
-ngrams <- readRDS("ngrams_2016_01_03.rds")
-make_table_batch(nbatches = 5, ngrams, nlength = 3)
+make_table_batch(nbatches = 5, ng, nlength = 3)
 dt <- read_batched_table("dt_2016-01-03_20_38")
 
 pred1 <- predict_next(dt, "Go on a romantic date at the")[1:100]
