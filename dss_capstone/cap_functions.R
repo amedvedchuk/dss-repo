55dacd9c268e8740476f23321f4c498fa6b13a09
unknown <sirius.nau@gmail.com>
2016-01-05 17:09:05 +0100

    implement batching for model creation

--- dss_capstone/cap_functions.R
+++ dss_capstone/cap_functions.R
@@ -4,6 +4,36 @@ library(quanteda)
 library(stringi)
 library(data.table)
 library(hashr)
+library(dplyr)
+
+predict_backoff <- function(dt, phrase, is_hash = FALSE){
+    # tokens <- unlist(tokenize(tolower("After years of work Semmi was vey tired"), removeNumbers = TRUE, removeTwitter = TRUE, ngrams = 3))
+    
+    
+    
+    tokens <- unlist(tokenize(tolower(phrase), removeNumbers = TRUE, removeTwitter = TRUE, removePunct = T,  ngrams = 2))
+    last_bigram <- tokens[length(tokens)]
+    print(last_bigram)
+    print(cat("last bigram:", last_bigram))
+    
+    if(is_hash){
+        last_bigram <- hash(last_bigram)
+    }
+    print(cat("last bigram:", last_bigram))
+    variants <- dt[prefix == last_bigram]
+    variants <- variants[order(variants$freq, decreasing = T),]
+    print(cat("found variants:", length(variants)))
+    variants
+    
+    #     getNgram <- function(tokens, ngram_len){
+    #         tlen <- length(tokens)
+    #         if(tlen > ngram_len){
+    #             tokens[tlen-ngram_len:ngram_len]
+    #         } else {
+    #             tokens
+    #         }
+    #     }
+}
 
 predict_next <- function(dt, phrase, is_hash = FALSE){
     # tokens <- unlist(tokenize(tolower("After years of work Semmi was vey tired"), removeNumbers = TRUE, removeTwitter = TRUE, ngrams = 3))
@@ -37,17 +67,26 @@ make_ngrams_batch <- function(nbatches = 1, subject, ...){
     do_work_batched(nbatches, subject, 
                     function(row, start_time,  ...){
                         print(row)
-                        wfm <- make_dfm(subject[row[1]:row[2]], ...)
-                        file_name <- paste("wfm_",format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
-                        print(paste("save batch to file: ", file_name))
-                        saveRDS(wfm, file_name)
-                        # i <<- i+1
-                        NULL
+                        
+                        ng <- list(...)$ngram
+                        
+                        replaced <- preproces_data(subject[row[1]:row[2]])
+                        
+                        sapply(ng, function(ngram){
+                            replaced <- make_dfm(replaced, ngram)
+                            file_name <- paste("wfm_ng",ngram,"_",format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
+                            print(paste("save batch to file: ", file_name))
+                            saveRDS(replaced, file_name)
+                            NULL
+                        })
+                        
                     }, ...)
 }
 
 make_ngrams <- function(lines, ngram){
-    replaced <- make_dfm(lines, ngram)
+    
+    replaced <- preproces_data(lines)
+    replaced <- make_dfm(replaced, ngram)
     print("start col sums...")
     replaced <- colSums(replaced)
     print("done!")
@@ -55,6 +94,26 @@ make_ngrams <- function(lines, ngram){
 
 make_dfm <- function(lines, ngram){
     
+    to_print <- system.time({
+        
+        print("tokenize to ngrams...")
+        replaced <- tokenize(toLower(lines), removeNumbers = TRUE, removeTwitter = TRUE, removePunct = T, ngrams = ngram)
+        print("head for tokens")
+        print(head(replaced, 5))
+        
+        # dfm_ng <- dfm(sentences, verbose = TRUE, toLower = TRUE, removeNumbers = TRUE, removeTwitter = TRUE, ngrams = ngram)
+        print(Sys.time())
+        print("start DFM creation....")
+        replaced <- dfm(replaced, toLower = F)
+        print("top features for DFM:")
+        print(topfeatures(replaced, 5))
+        
+    })
+    print(to_print)
+    replaced
+}
+
+preproces_data <- function(lines){
     to_print <- system.time({
         # remove non ASCII
         print(Sys.time())
@@ -80,19 +139,7 @@ make_dfm <- function(lines, ngram){
         #         print(Sys.time())
         print(cat("sentences length: ", length(replaced)))
         print("head for sentences:")
-        print(head(replaced, 20))
-        
-        print("tokenize to ngrams...")
-        tokens <- tokenize(toLower(replaced), removeNumbers = TRUE, removeTwitter = TRUE, removePunct = T, ngrams = ngram)
-        print("head for tokens")
-        print(head(tokens, 30))
-        
-        # dfm_ng <- dfm(sentences, verbose = TRUE, toLower = TRUE, removeNumbers = TRUE, removeTwitter = TRUE, ngrams = ngram)
-        print(Sys.time())
-        print("start DFM creation....")
-        replaced <- dfm(tokens, toLower = F)
-        print("top features for DFM:")
-        print(topfeatures(replaced, 30))
+        print(head(replaced, 10))
         
     })
     print(to_print)
@@ -106,7 +153,7 @@ merge_word_fm <- function(dfm1, dfm2){
     if(is.dfm(dfm2)){
         dfm2 <- colSums(dfm2)
     }
-   
+    
     ind <- intersect(names(dfm1), names(dfm2))
     diff1 <- setdiff(names(dfm1), names(dfm2))
     diff2 <- setdiff(names(dfm2), names(dfm1))
@@ -126,28 +173,13 @@ do_work_batched <- function(nbatches = 1, subject, FUN, ...){
 }
 
 make_table_batch <- function(nbatches = 1, subject, ...){
-    #     batches <- c(0, round(1:nbatches*(length(subject)/nbatches)))
-    #     # batches <- c(0, round(1:3*(1500/3)))
-    #     start_time <- Sys.time()
-    #     
-    #     batches <- data.frame(start = batches[1:nbatches]+1, end=batches[2:(nbatches+1)])
-    #     print(batches)
-    #    
-    #     i <- 1
-    #     apply(batches, 1, function(row){
-    #         print(row)
-    #         dt <- make_table(subject[row[1]:row[2]], ...)
-    #         file_name <- paste("dt_",format(start_time, "%Y-%m-%d_%H_%M_"), i, ".rds", sep = "")
-    #         print(paste("save batch to file: ", file_name))
-    #         saveRDS(dt, file_name)
-    #         i <<- i+1
-    #         NULL
-    #     })
+    
     do_work_batched(nbatches, subject, 
                     function(row, start_time,  ...){
                         print(row)
+                        nl <- list(...)$nlength
                         dt <- make_table(subject[row[1]:row[2]], ...)
-                        file_name <- paste("dt_",format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
+                        file_name <- paste("dt_nl", nl, "_" ,format(start_time, "%Y-%m-%d_%H_%M_"), row[3], ".rds", sep = "")
                         print(paste("save batch to file: ", file_name))
                         saveRDS(dt, file_name)
                         # i <<- i+1
@@ -157,11 +189,43 @@ make_table_batch <- function(nbatches = 1, subject, ...){
 }
 
 read_batched_wfm <- function(prefix){
+    print(paste("read batched WFM by prefix: ", prefix))
     files <- grep(prefix, list.files("."), value = T)
-    dt <- readRDS(files[1])
-    sapply(files[2:length(files)], function(file){
-        dt <<- merge_word_fm(dt, readRDS(file))
-    })
+    
+    dt <- NULL
+    
+    if(length(files) == 1){
+        
+        print(paste("readfile: ", files[1]))
+        dt <- readRDS(files[1])
+        
+    } else {
+        
+        ng_bloks <- stri_extract_last_regex(files, pattern = "ng.", simplify = T) %>% unique()
+        print(ng_bloks)
+        
+        if(length(ng_bloks) == 1){
+            print(paste("readfile: ", files[1]))
+            dt <- readRDS(files[1])
+            sapply(files[2:length(files)], function(file){
+                print(paste("readfile: ", file))
+                dt <<- merge_word_fm(dt, readRDS(file))
+            })
+        } else {
+            
+            print("detected multiple blocks...")
+            dt <- list()
+            sapply(ng_bloks, function(block){
+                files_in_block <- grep(block, files, value = T)
+                dt_in_blok <- readRDS(files_in_block[1])
+                sapply(files_in_block[2:length(files_in_block)], function(file){
+                    print(paste("readfile: ", file))
+                    dt_in_blok <<- merge_word_fm(dt_in_blok, readRDS(file))
+                })
+                dt[[block]] <<- dt_in_blok
+            })  
+        }
+    }
     dt
 }
 
@@ -179,13 +243,29 @@ make_table <- function(ngrams, nlength = 2, is_hash = FALSE){
     
     to_print <- system.time({
         print("start table preparation...")
-        splitted <- stri_split_fixed(names(ngrams), pattern = "_", simplify = T)
-        ulen <- dim(splitted)[2] 
-        print(paste(c("rows", "cols"), dim(splitted), collapse = " "))
-        final_dt <- data.table(prefix = apply(splitted[,1:nlength-1], 1, stri_c, collapse="_"),
-                               lastw = splitted[,nlength],
-                               freq = ngrams
-        )
+        
+        if(nlength > 2){
+            splitted <- stri_split_fixed(names(ngrams), pattern = "_", simplify = T)
+            ulen <- dim(splitted)[2] 
+            print(paste(c("rows", "cols"), dim(splitted), collapse = " "))
+            final_dt <- data.table(prefix = apply(splitted[,1:nlength-1], 1, stri_c, collapse="_"),
+                                   lastw = splitted[,nlength],
+                                   freq = ngrams
+            )
+        } else if (nlength == 2) {
+            splitted <- stri_split_fixed(names(ngrams), pattern = "_", simplify = T)
+            ulen <- dim(splitted)[2] 
+            print(paste(c("rows", "cols"), dim(splitted), collapse = " "))
+            final_dt <- data.table(prefix = splitted[,1],
+                                   lastw = splitted[,nlength],
+                                   freq = ngrams
+            )
+        } else if (nlength == 1) {
+            final_dt <- data.table(prefix = names(ngrams),
+                                   freq = ngrams
+            )
+        }
+        
         if(is_hash){
             print("start hashing...")
             
@@ -195,8 +275,6 @@ make_table <- function(ngrams, nlength = 2, is_hash = FALSE){
         }
         setkey(final_dt, prefix)
         print("done!")
-        
-        
         
         # final_dt
     })
--- dss_capstone/run_performance.R
+++ dss_capstone/run_performance.R
@@ -7,13 +7,30 @@ total <- c(read_file("final/en_US/en_US.blogs.txt"),
            read_file("final/en_US/en_US.news.txt"),
            read_file("final/en_US/en_US.twitter.txt"))
 
-make_ngrams_batch(nbatches = 10, total, ngram = 3)
-
-ng <- read_batched_wfm("wfm_2016-01-05_00_52")
+make_ngrams_batch(nbatches = 5, total, ngram = c(1:4))
 
 
-make_table_batch(nbatches = 5, ng, nlength = 3)
-dt <- read_batched_table("dt_2016-01-03_20_38")
+ngb <- read_batched_wfm("wfm_ng._2016-01-05_14_09")
+# is_hash = FALSE 
+make_table_batch(nbatches = 5, ngb$ng1, nlength = 1)
+make_table_batch(nbatches = 5, ngb$ng2, nlength = 2)
+make_table_batch(nbatches = 5, ngb$ng3, nlength = 3)
+make_table_batch(nbatches = 5, ngb$ng4, nlength = 4)
+
+dtl <- list(
+    dt1 = read_batched_table("dt_nl1_2016-01-05_16"),
+    dt2 = read_batched_table("dt_nl2_2016-01-05_16"),
+    dt3 = read_batched_table("dt_nl3_2016-01-05_16"),
+    dt4 = read_batched_table("dt_nl4_2016-01-05_16")
+)
+
+
+
+ng1 <- read_batched_wfm("wfm_2016-01-05_12_33")
+make_table_batch(nbatches = 5, ng1, nlength = 3)
+dt3 <- read_batched_table("dt_nl3_2016-01-05_15_06")
+dt3h <- dt3
+dt3h$prefix <- sapply(dt3h$prefix, hash)
 
 pred1 <- predict_next(dt, "Go on a romantic date at the")[1:100]
 pred1$rel_freq <- pred1$freq/sum(ngrams)
